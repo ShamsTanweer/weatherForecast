96b79983235665408017e5dec91b7b7404e5f215
ShamsTanweer <82506094+ShamsTanweer@users.noreply.github.com>
2021-11-17 01:49:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+   
+});
+
+
+
+
+
+app.post("/", function(req, res){
+
+
+
+    const query = req.body.CityName
+const apikey = "56b9cf140bcd6586c24359c7216f3ed5"
+const unit = "metric"
+
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apikey+""
+
+https.get(url, function (response){
+    
+
+
+    response.on("data", function (data){
+       const weatherData = JSON.parse(data);
+       const temp = weatherData.main.temp
+       const WeatherDescription = weatherData.weather[0].description
+       const cName = weatherData.name
+       const icon = weatherData.weather[0].icon
+       const imageurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+
+       
+       res.write("<h1>The temperature in " +cName  +" is "+ temp + " degrees celcius.</h1>");
+       res.write("<h2>The weather is currently " + WeatherDescription + "</h2>");
+       res.write("<img src="+imageurl+">")
+       res.send();
+    });
+
+
+});
+
+
+    console.log(req.body.CityName);
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080, function(){
+    console.log("Server is running on port 8080");
+});
