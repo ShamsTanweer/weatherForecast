ae19d6f35f9cafdb634e148a5c1a07a7609e139d
ShamsTanweer <82506094+ShamsTanweer@users.noreply.github.com>
2021-11-17 22:22:27 +0530

    CSS problem resolving

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/Public'));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
